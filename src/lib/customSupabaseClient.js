ef32265e605c67f6f5532ee5fcec239387a1412c
VenatorVictor <venator.victor@protonmail.com>
2026-05-27 08:53:25 -0300

    Security Fix

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lxfvayyapmmoaumiyrka.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4ZnZheXlhcG1tb2F1bWl5cmthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0NTkxMTgsImV4cCI6MjA4NTAzNTExOH0.x_QSBh5pciEJdMyZPROnF7Iw2jEl_IxVzv7Q6amgtRg';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
